8e6fbaa803054197f2836c29cd5fc1f89be121db
Nikita Prytkov <prytkov.nikita.set@gmail.com>
2020-05-17 10:02:02 +0400

    Добавляет пользовательские маршруты

--- src/express/index.js
+++ src/express/index.js
@@ -6,10 +6,12 @@ const chalk = require(`chalk`);
 const { DEFAULT_PORT } = require(`./constants`);
 const mainRouter = require(`./routes/main`);
 const authenticationRouter = require(`./routes/authentication`);
+const userRouter = require(`./routes/user`);
 
 const app = express();
 
 app.use(mainRouter);
 app.use(authenticationRouter);
+app.use(`/my`, userRouter);
 
 app.listen(DEFAULT_PORT, () => console.info(chalk.green(`Принимаю подключения на ${ DEFAULT_PORT }`)))
