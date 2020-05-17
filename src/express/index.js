1201f3040367eb14f8fb578a3a600a444f8fccdf
Nikita Prytkov <prytkov.nikita.set@gmail.com>
2020-05-17 10:10:37 +0400

    Добавляет маршруты для категорий

--- src/express/index.js
+++ src/express/index.js
@@ -8,6 +8,7 @@ const mainRouter = require(`./routes/main`);
 const authenticationRouter = require(`./routes/authentication`);
 const userRouter = require(`./routes/user`);
 const articlesRouter = require(`./routes/articles`);
+const categoriesRouter = require(`./routes/categories`);
 
 const app = express();
 
@@ -15,5 +16,6 @@ app.use(mainRouter);
 app.use(authenticationRouter);
 app.use(`/my`, userRouter);
 app.use(`/articles`, articlesRouter);
+app.use(`/categories`, categoriesRouter);
 
 app.listen(DEFAULT_PORT, () => console.info(chalk.green(`Принимаю подключения на ${ DEFAULT_PORT }`)))
