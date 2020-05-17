20ffad44dea7d1e464529cb2aa874b754cb94c0a
Nikita Prytkov <prytkov.nikita.set@gmail.com>
2020-05-17 10:07:49 +0400

    Добавляет маршруты для статей

--- src/express/index.js
+++ src/express/index.js
@@ -7,11 +7,13 @@ const { DEFAULT_PORT } = require(`./constants`);
 const mainRouter = require(`./routes/main`);
 const authenticationRouter = require(`./routes/authentication`);
 const userRouter = require(`./routes/user`);
+const articlesRouter = require(`./routes/articles`);
 
 const app = express();
 
 app.use(mainRouter);
 app.use(authenticationRouter);
 app.use(`/my`, userRouter);
+app.use(`/articles`, articlesRouter);
 
 app.listen(DEFAULT_PORT, () => console.info(chalk.green(`Принимаю подключения на ${ DEFAULT_PORT }`)))
