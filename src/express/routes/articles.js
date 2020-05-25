8f5f38fd0d5959cdde64db6763b2ba4c7deabfbf
Nikita Prytkov <prytkov.nikita.set@gmail.com>
2020-05-25 13:00:37 +0400

    Изменяет отправку строки на отправку шаблона для маршрута "/articles/edit/:id"

--- src/express/routes/articles.js
+++ src/express/routes/articles.js
@@ -6,7 +6,7 @@ const router = new Router();
 
 router.get(`/add`, (req, res) => res.render(`articles/new-post`));
 router.get(`/:id`, (req, res) => res.render(`articles/post`));
-router.get(`/edit/:id`, (req, res) => res.send(`/articles/edit/:id`));
+router.get(`/edit/:id`, (req, res) => res.render(`articles/new-post`));
 router.get(`/category/:id`, (req, res) => res.render(`articles/articles-by-category`));
 
 module.exports = router;
